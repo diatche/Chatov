3e7e3790638393a4c79238cd34758ff749aa2130
Pavel Diatchenko <diatche@gmail.com>
2016-08-05 11:51:58 +0300

    Initial Commit

---
+++ Chatov/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chatov
+//
+//  Created by Pavel Diatchenko on 5/08/16.
+//  Copyright © 2016 Pavel Diatchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
