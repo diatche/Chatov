cb86ecdfb35e3db6e06ed74733dfa0c482ae8451
Pavel Diatchenko <diatche@gmail.com>
2016-08-05 13:52:15 +0300

    Setting up message table view.

--- Chatov/ViewController.swift
+++ Chatov/ViewController.swift
@@ -10,16 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView : UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupTableView()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setupTableView() {
+
     }
-
-
 }
 
