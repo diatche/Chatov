86daa999d1f3b591b37b3524f2bf088777c5d164
Pavel Diatchenko <diatche@gmail.com>
2016-08-05 12:44:50 +0300

    Added Firebase pods and configuration.

---
+++ Pods/Target Support Files/Pods-Chatov/Pods-Chatov-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Chatov : NSObject
+@end
+@implementation PodsDummy_Pods_Chatov
+@end
---
+++ Pods/Target Support Files/Pods-Chatov/Pods-Chatov-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ChatovVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ChatovVersionString[];
+
